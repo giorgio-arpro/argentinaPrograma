f05374118e380383d43c834084fd915991a1d22c
giorgio-arpro <flacobass@gmail.com>
2023-06-20 15:29:05 -0300

    Practico N0 finalizado

---
+++ practico0/promedio3.js
@@ -0,0 +1,19 @@
+let num_x1 = 5;
+let num_y1 = 7;
+let num_z1 = 9;
+
+let num_x2 = -2;
+let num_y2 = -3;
+let num_z2 = -8;
+
+let num_x3 = -1.2;
+let num_y3 = 5.3;
+let num_z3 = -33;
+
+let promedio1 = (num_x1+num_y1+num_z1)/3;
+let promedio2 = (num_x2+num_y2+num_z2)/3;
+let promedio3 = (num_x3+num_y3+num_z3)/3;
+
+console.log("Promedio del punto 4a es: "+promedio1);
+console.log("Promedio del punto 4b es: "+promedio2);
+console.log("Promedio del punto 4c es: "+promedio3);
