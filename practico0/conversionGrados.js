4a5a05d3883c279399be3c7aeb225a3b75131eb5
giorgio-arpro <flacobass@gmail.com>
2023-06-20 15:26:07 -0300

    Agrego el archivo conversionGrados.js

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,6 @@
+let fahreng1 = 50;
+let fahreng2 = 75;
+let celsius1 = (fahreng1-32)*(5/9);
+let celsius2 = (fahreng2-32)*(5/9);
+console.log("Grados celsius del punto 3a: "+celsius1+"C");
+console.log("Grados celsius del punto 3b: "+celsius2+"C");
