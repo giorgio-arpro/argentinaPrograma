d9f84e7934895bb34d8db905a5c33826e6b466f1
giorgio-arpro <flacobass@gmail.com>
2023-06-20 17:51:02 -0300

    parte 2, ejercicio 5

---
+++ practico1/frutas.js
@@ -0,0 +1,8 @@
+let fruta =[];
+let otraFruta="uva";
+fruta.push("banana");
+fruta.push("pera","manzana","kiwi",otraFruta);
+let ultima = fruta[fruta.length-1];
+console.log("La ultima fruta agregada fue: "+ultima);
+let primera = fruta[0];
+console.log("La primera fruta agregada fue: "+primera);
